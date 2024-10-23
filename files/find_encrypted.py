570af6bac23a0db4bda7c9b5429e648730fd81ba
Henry Burgess <henryjburg@gmail.com>
2024-10-23 12:45:06 -0500

    Update comments for `find_encrypted`

--- files/find_encrypted.py
+++ files/find_encrypted.py
@@ -18,17 +18,27 @@ def start():
     for file in sorted(files):
       file_path = os.path.join(subdir, file)
       opened_file = open(file_path, "rb")
+
       with opened_file:
+        # Read the contents of the file and parse directly to a string
         contents = str(opened_file.read())
+
+        # This is a hack to tell if a Microsoft XML-like format file is encrypted
         if "<encryption" in contents and "</encryption>" in contents:
           encrypted_file_paths.append(file_path)
+
       opened_file.close()
       file_count += 1
+
+      # Update the progress text
       print("Searching... \t\t\t " + str(file_count) + " files", end="\r", flush=True)
 
+  # Generate and print a summary
   print("\nDone.")
-  print("Reviewed " + str(file_count) + " files, found " + str(len(encrypted_file_paths)) + " encrypted files:")
-  print("\n".join(encrypted_file_paths))
+  print("Reviewed " + str(file_count) + " files, found " + str(len(encrypted_file_paths)) + " encrypted files.")
+  if len(encrypted_file_paths) > 0:
+    print("Files:")
+    print("\n".join(encrypted_file_paths))
 
 
 if __name__ == "__main__":
